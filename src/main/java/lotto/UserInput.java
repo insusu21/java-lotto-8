a5d11761dc53e9bb2c8af9adff562ee48934ec24
insu <dlstn0201@daum.net>
2025-10-31 14:09:12 +0900

    feat: 구입 금액 입력받는 기능

--- src/main/java/lotto/UserInput.java
+++ src/main/java/lotto/UserInput.java
@@ -3,6 +3,18 @@ package lotto;
 import camp.nextstep.edu.missionutils.Console;
 
 public class UserInput {
+    public int inputMoney() {
+        while (true) {
+            String input = Console.readLine();
+            try{
+                int paid = parseAndValidateInput(input);
+                return paid;
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+
     private void validatePaid(int paid) {
         if (paid % 1000 != 0) {
             throw new IllegalArgumentException("[ERROR] 1000 단위의 숫자를 입력해주세요.");
