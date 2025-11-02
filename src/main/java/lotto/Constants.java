0b8fd1832d85ba56ef609a62a9e14675beb50ba4
insu <dlstn0201@daum.net>
2025-11-02 23:02:58 +0900

    feat: 상수 추가

--- src/main/java/lotto/Constants.java
+++ src/main/java/lotto/Constants.java
@@ -10,12 +10,13 @@ public class Constants {
 
     public static final String PURCHASE_MESSAGE = "%d개를 구매했습니다.";
 
-    public static final String MONEY_INPUT_MESSAGE = "구입금액을 입력해 주세요.";
-
+    public static final String INPUT_MONEY_MESSAGE = "구입금액을 입력해 주세요.";
+    public static final String INPUT_WINNING_NUMBER_MESSAGE = "당첨 번호를 입력해 주세요.";
+    public static final String INPUT_BONUS_NUMBER_MESSAGE = "보너스 번호를 입력해 주세요.";
     public static final String INVALID_ERROR_MESSAGE = "[ERROR] 1000 단위의 숫자를 입력해주세요.";
     public static final String LOTTO_SIZE_ERROR_MESSAGE = "[ERROR] 로또 번호는 6개여야 합니다.";
-    public static final String NUMBER_ERROR_MESSAGE = "[ERROR] 1-45 사이의 서로 다른 숫자 6개를 입력해주세요.";
-
+    public static final String WINNING_NUMBER_ERROR_MESSAGE = "[ERROR] 1-45 사이의 서로 다른 숫자 6개를 입력해주세요.";
+    public static final String BONUS_NUMBER_ERROR_MESSAGE = "[ERROR] 1-45 사이의 당첨 숫자와 다른 숫자 1개를 입력해주세요.";
 
 }
 
