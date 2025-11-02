70c10454a32090f45e037e728cae90a983c561e2
insu <dlstn0201@daum.net>
2025-11-02 16:13:22 +0900

    feat: 장수만큼 로또 출력해서 전체 반환 기능 구현

---
+++ src/main/java/lotto/LottoStorage.java
@@ -0,0 +1,21 @@
+package lotto;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LottoStorage {
+    public List<Lotto> lottoStore(int lottoAmount) {
+        List<Lotto> totalLotto = new ArrayList<>();
+        RandomNumber randomNumber = new RandomNumber();
+
+        for (int i = 0; i < lottoAmount; i++) {
+            List<Integer> numbers = randomNumber.randomNumberGenerator();
+
+            Lotto lotto = new Lotto(numbers);
+            lotto.lottoSort();
+
+            totalLotto.add(lotto);
+        }
+        return totalLotto;
+    }
+}
