d117fafc4d87782c3f7de11a9e78f16dbc835061
insu <dlstn0201@daum.net>
2025-11-02 16:14:27 +0900

    test: 장수만큼 로또 출력해서 전체 반환 기능 테스트코드 추가

---
+++ src/test/java/lotto/LottoStorageTest.java
@@ -0,0 +1,20 @@
+package lotto;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class LottoStorageTest {
+    @Test
+    void 로또는_요청한_장수만큼_반환한다() {
+        LottoStorage lottoStorage = new LottoStorage();
+        int lottoAmount = 5;
+
+        List<Lotto> Lottos = lottoStorage.lottoStore(lottoAmount);
+
+        assertThat(Lottos).hasSize(5);
+    }
+
+}
