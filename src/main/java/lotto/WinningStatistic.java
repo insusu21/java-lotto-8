b7d6fdb8132332e42f81c107159507c6290122e3
insu <dlstn0201@daum.net>
2025-11-03 17:35:18 +0900

    feat: 수익률 계산기능 구현

--- src/main/java/lotto/WinningStatistic.java
+++ src/main/java/lotto/WinningStatistic.java
@@ -9,4 +9,20 @@ public class WinningStatistic {
             lottoPrize.increaseCount();
         }
     }
+
+    public long calculateTotalPrize() {
+        long totalPrize = 0L;
+        for (LottoPrize prize : LottoPrize.values()) {
+            if (prize == LottoPrize.LOSE) continue;
+            totalPrize += ((long) prize.getPrize() * prize.getCount());
+        }
+        return totalPrize;
+    }
+
+    public double calculateRateOfReturn(int paid, long totalPrize) {
+        if (paid == 0) {
+            return 0.0;
+        }
+        return ((double) totalPrize / (double) paid) * 100.0;
+    }
 }
