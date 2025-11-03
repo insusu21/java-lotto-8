0564cd92510e55d19f9ce114cf35a7a04c09ef23
insu <dlstn0201@daum.net>
2025-11-03 17:35:32 +0900

    feat: 당첨 통계 출력 기능 구현

--- src/main/java/lotto/OutputView.java
+++ src/main/java/lotto/OutputView.java
@@ -11,4 +11,19 @@ public class OutputView {
             System.out.println(lotto.getNumbers());
         }
     }
+
+    public void printWinningStatistic(double rateOfReturn) {
+        System.out.println(Constants.STATISTICS_HEADER);
+        for (LottoPrize prize : LottoPrize.values()) {
+            if (prize == LottoPrize.LOSE) continue;
+
+            String bonusText = "";
+            if (prize.isBonusRequired()) {bonusText = Constants.BONUS_MATCH_TEXT;}
+
+            System.out.printf(
+                    Constants.STATISTICS_FORMAT, prize.getMatchCount(), bonusText, prize.getPrize(), prize.getCount());
+        }
+        System.out.printf(Constants.RATE_OF_RETURN_MESSAGE, rateOfReturn);
+    }
+
 }
