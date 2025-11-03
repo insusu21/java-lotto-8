f80f3019e1b7d17f1e9e7c8a7ebeeeae0e58897c
insu <dlstn0201@daum.net>
2025-11-03 17:30:26 +0900

    feat: 상수 추가

--- src/main/java/lotto/Constants.java
+++ src/main/java/lotto/Constants.java
@@ -9,12 +9,18 @@ public class Constants {
     public static final int LOTTO_PRICE_UNIT = 1000;
 
     public static final String PURCHASE_MESSAGE = "%d개를 구매했습니다.";
+    public static final String STATISTICS_HEADER = "\n--- 당첨 통계 ---";
+    public static final String STATISTICS_FORMAT = "%d개 일치%s (%,d원) - %d개\n";
+    public static final String BONUS_MATCH_TEXT = ", 보너스 볼 일치";
+    public static final String RATE_OF_RETURN_MESSAGE = "총 수익률은 %.1f%%입니다.\n";
 
     public static final String INPUT_MONEY_MESSAGE = "구입금액을 입력해 주세요.";
     public static final String INPUT_WINNING_NUMBER_MESSAGE = "당첨 번호를 입력해 주세요.";
     public static final String INPUT_BONUS_NUMBER_MESSAGE = "보너스 번호를 입력해 주세요.";
+
+
     public static final String INVALID_ERROR_MESSAGE = "[ERROR] 1000 단위의 숫자를 입력해주세요.";
-    public static final String LOTTO_SIZE_ERROR_MESSAGE = "[ERROR] 로또 번호는 6개여야 합니다.";
+    public static final String LOTTO_ERROR_MESSAGE = "[ERROR] 로또 번호는 1-45 사이의 서로 다른 숫자 6개여야 합니다.";
     public static final String WINNING_NUMBER_ERROR_MESSAGE = "[ERROR] 1-45 사이의 서로 다른 숫자 6개를 입력해주세요.";
     public static final String BONUS_NUMBER_ERROR_MESSAGE = "[ERROR] 1-45 사이의 당첨 숫자와 다른 숫자 1개를 입력해주세요.";
 
